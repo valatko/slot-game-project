4b12f7bc34bbd4d3d5e46435502f0df20a1d6252
Vladimir Zhelezarski <vladimir.jelezarsky@gmail.com>
2026-03-26 18:45:52 +0200

    feat: implement slot size, multiple paylines, refactor of evaluation functions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include<random>
 #include<vector>
-int pick_random_symbol(const std::vector<int>& reel, std::mt19937& gen) {
-    std::uniform_int_distribution<> dist(0, reel.size()-1);
-    int index = dist(gen);
-    return reel[index];
-}
+const int ROWS = 3;
+const int COLUMNS =3;
+const std::vector<std::vector<int>> paylines = {
+    {0,0,0},
+    {1,1,1},
+    {2,2,2},
+    {0,1,2},
+    {2,1,0}
+};
 const std::vector<std::vector<int>> payout_table={
     {0,0,10},
     {0,0,10},
@@ -19,6 +23,11 @@ const std::vector<std::vector<int>> reels ={
     {1,1,1,2,2,3,3,4,5,6},
     {1,1,1,2,2,3,3,4,5,6}
 };
+int pick_random_stop(const std::vector<int>& reel, std::mt19937& gen) {
+    std::uniform_int_distribution<> dist(0, reel.size()-1);
+    int stop = dist(gen);
+    return stop;
+}
 int length_of_win_line(const std::vector<int>& spin_result){
     int length=1;
     for (int i=1;i<spin_result.size();i++){
@@ -31,16 +40,40 @@ int length_of_win_line(const std::vector<int>& spin_result){
     }
     return length;
 }
+std::vector<int> get_visible_window(const std::vector<int>& reel, int stop_index){
+    std::vector<int> window;
+    for (int i=0; i<ROWS;i++){
+        window.push_back(reel[(stop_index+i)%reel.size()]);
+    }
+    return window;
+}
+std::vector<std::vector<int>> spin_screen(const std::vector<std::vector<int>>& reels, std::mt19937& gen){
+    std::vector<std::vector<int>> screen;
+    for(int i=0;i<reels.size();i++){
+        int stop = pick_random_stop(reels[i],gen);
+        std::vector<int> window = get_visible_window(reels[i],stop);
+        screen.push_back(window);
+    }
+    return screen;
+}
+std::vector<int> get_payline(const std::vector<std::vector<int>>& screen,const std::vector<int>& line_definition){
+    std::vector<int> payline;
+    for(int i=0;i<line_definition.size();i++){
+        payline.push_back(screen[i][line_definition[i]]);
+    }
+    return payline;
+}
 int get_payout(const std::vector<int>& spin_result){
     return payout_table[spin_result[0]-1][length_of_win_line(spin_result)-1];
 }
-std::vector<int> spin(const std::vector<std::vector<int>>& reels, std::mt19937& gen){
-    std::vector<int> spin_result;
-    for(int j=0;j<reels.size();j++){
-            int symbol=pick_random_symbol(reels[j],gen);
-            spin_result.push_back(symbol);
-        }
-    return spin_result;
+int get_total_payout_for_screen(const std::vector<std::vector<int>>& screen, const std::vector<std::vector<int>>& paylines){
+    int total_payout = 0;
+    for(int i=0;i<paylines.size();i++){
+        std::vector<int> payline = get_payline(screen,paylines[i]);
+        int line_payout = get_payout(payline);
+        total_payout += line_payout;
+    }
+    return total_payout;
 }
 struct SimulationStats {
     int total_number_of_bets=0;
@@ -49,8 +82,8 @@ struct SimulationStats {
     double hit_rate=0;
     double RTP=0;
 };
-void process_spin(const std::vector<int>& spin_result, SimulationStats& stats){
-    int spin_payout=get_payout(spin_result);
+void process_spin(const std::vector<std::vector<int>>& spin_screen, SimulationStats& stats){
+    int spin_payout=get_total_payout_for_screen(spin_screen,paylines);
     stats.total_payout+=spin_payout;
     if(spin_payout!=0){
         stats.matches++;
@@ -60,11 +93,12 @@ void process_spin(const std::vector<int>& spin_result, SimulationStats& stats){
 SimulationStats run_simulation(const std::vector<std::vector<int>>& reels, int number_of_spins,std::mt19937& gen){
     SimulationStats stats;
     for(int i=0;i<number_of_spins;i++){
-        std::vector<int> spin_result = spin(reels,gen);
+        std::vector<std::vector<int>> spin_result = spin_screen(reels,gen);
         process_spin(spin_result,stats);
     }
     stats.hit_rate=static_cast<double>(stats.matches)/stats.total_number_of_bets;
     stats.RTP=stats.total_payout/stats.total_number_of_bets;
+    
     return stats;
 }
 int main(){
